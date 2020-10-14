caa665311404307c83a19f5c43847210f7beba68
Anastasiia Honcharova <72573939+anastasiia2610@users.noreply.github.com>
2020-10-14 15:59:08 +0300

    Add files via upload

--- HillelHW/Honcharova_HW4/js/script.js
+++ HillelHW/Honcharova_HW4/js/script.js
@@ -1,62 +1,48 @@
 'use strict'
 
-const beerOffering1 = confirm('Would you like to drink some beer?');
+let beerMessage1;
 
 if (beerOffering1) {
-    alert('OK, we have a gift for you - free snacks to beer!');
+   beerMessage1 = 'OK, we have a gift for you - free snacks to beer!';
 } else {
-    alert('Thanks for your attention, see you next time!');
+   beerMessage1 = 'Thanks for your attention, see you next time!';
 }
 
 
-const beerOffering2 = confirm('Would you like to drink some beer?');
-
-const beerAgreement = beerOffering2 ? 'OK, we have a gift for you - free snacks to beer!' : 'Thanks for your attention, see you next time!';
-
-alert(beerAgreement);
+let beerMessage2 = beerOffering2 ? 'OK, we have a gift for you - free snacks to beer!' :
+'Thanks for your attention, see you next time!';
 
 
 
-const userAge1 = prompt('How old are you?');
+let ageMessage1;
 
 if (userAge1 < 18) {
-    alert('Sorry, you have not reached legal age yet!');
+    ageMessage1 = 'Sorry, you have not reached legal age yet!';
 } else if (userAge1 > 30){
-    alert('Welcome to our web-site!');
+    ageMessage1 = 'Welcome to our web-site!';
 } else {
-    alert('Yo, whats up?');
+    ageMessage1 = 'Yo, whats up?';
 }
 
 
-const userAge2 = prompt('How old are you?');
-
-const legalAge = (userAge2 < 18) ? 'Sorry, you have not reached legal age yet!' : (userAge2 > 30) ? 'Welcome to our web-site!' : 'Yo, whats up?';
-
-alert(legalAge);
+let ageMessage2 = (userAge2 < 18) ? 'Sorry, you have not reached legal age yet!' : 
+(userAge2 > 30) ? 'Welcome to our web-site!' : 'Yo, whats up?';
 
 
 
-const drinkChoosing1 = prompt('Would you like some coffee or some tea?', 'tea');
-
-alert(drinkChoosing1);
-
-const addSugar1 = confirm('Do you drink with added sugars?');
+let drinkChoosingMessage1;
 
 if (addSugar1){
-    alert(`Thank you for choosing our cafe! Your order: a cup of ${drinkChoosing1} with sugar!`);
+   drinkChoosingMessage1 = `Thank you for choosing our cafe! Your order: a cup of ${drinkChoosingMessage1} with sugar!`;
 } else {
-    alert(`Thank you for choosing our cafe! Your order : a cup of ${drinkChoosing1}!`);
+    drinkChoosingMessage1 = `Thank you for choosing our cafe! Your order : a cup of ${drinkChoosingMessage1}!`;
 } 
 
 
-const drinkChoosing2 = prompt('Would you like some coffee or some tea?', 'tea');
+let drinkChoosingMessage2 = addSugar2 ? `Thank you for choosing our cafe! Your order: a cup of ${drinkChoosingMessage2} with sugar!` :
+`Thank you for choosing our cafe! Your order : a cup of ${drinkChoosingMessage2}`;
 
-alert(drinkChoosing2);
 
-const addSugar2 = confirm('Do you drink with added sugars?');
 
-const userOrder = true ? `Thank you for choosing our cafe! Your order: a cup of ${drinkChoosing1} with sugar!` : `Thank you for choosing our cafe! Your order : a cup of ${drinkChoosing1}`;
-
-alert(userOrder);
 
 
