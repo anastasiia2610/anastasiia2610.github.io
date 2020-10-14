9bfd21abf566b29c89f60cffbac6926266f55423
Anastasiia Honcharova <72573939+anastasiia2610@users.noreply.github.com>
2020-10-14 17:37:29 +0300

    Add files via upload

--- HillelHW/Honcharova_HW4/js/script.js
+++ HillelHW/Honcharova_HW4/js/script.js
@@ -3,15 +3,15 @@
 let beerMessage1;
 
 if (beerOffering1) {
-   beerMessage1 = 'OK, we have a gift for you - free snacks to beer!';
+    beerMessage1 = 'OK, we have a gift for you - free snacks to beer!';
 } else {
-   beerMessage1 = 'Thanks for your attention, see you next time!';
+    beerMessage1 = 'Thanks for your attention, see you next time!';
 }
 
 
-let beerMessage2 = beerOffering2 ? 'OK, we have a gift for you - free snacks to beer!' :
-'Thanks for your attention, see you next time!';
-
+let beerMessage2 = beerOffering2 ? 
+    'OK, we have a gift for you - free snacks to beer!' :
+    'Thanks for your attention, see you next time!'; 
 
 
 let ageMessage1;
@@ -25,22 +25,34 @@ if (userAge1 < 18) {
 }
 
 
-let ageMessage2 = (userAge2 < 18) ? 'Sorry, you have not reached legal age yet!' : 
-(userAge2 > 30) ? 'Welcome to our web-site!' : 'Yo, whats up?';
+let ageMessage2 = (userAge2 < 18) ?
+    'Sorry, you have not reached legal age yet!' : 
+    (userAge2 > 30) ?
+    'Welcome to our web-site!' : 
+    'Yo, whats up?';
 
 
 
 let drinkChoosingMessage1;
 
-if (addSugar1){
-   drinkChoosingMessage1 = `Thank you for choosing our cafe! Your order: a cup of ${drinkChoosingMessage1} with sugar!`;
+if (choosingTea1){
+    drinkChoosingMessage1 = 'Thank you for choosing our cafe! Your order: a cup of tea!';
+} else if (choosingCoffee1) {
+    drinkChoosingMessage1 = 'Thank you for choosing our cafe! Your order : a cup of coffee!';
+} else if (choosingTea1 && addSugar1) {
+    drinkChoosingMessage1 = 'Thank you for choosing our cafe! Your order: a cup of tea with sugar!'
 } else {
-    drinkChoosingMessage1 = `Thank you for choosing our cafe! Your order : a cup of ${drinkChoosingMessage1}!`;
-} 
+    'Thank you for choosing our cafe! Your order: a cup of coffe with sugar!'
+}
 
 
-let drinkChoosingMessage2 = addSugar2 ? `Thank you for choosing our cafe! Your order: a cup of ${drinkChoosingMessage2} with sugar!` :
-`Thank you for choosing our cafe! Your order : a cup of ${drinkChoosingMessage2}`;
+let drinkChoosingMessage2 = choosingTea1 ?
+    'Thank you for choosing our cafe! Your order : a cup of tea!':
+    choosingCoffee1 ? 
+    'Thank you for choosing our cafe! Your order : a cup of coffee!' :
+    choosingTea1 && addSugar1 ?
+    'Thank you for choosing our cafe! Your order: a cup of tea with sugar!' :
+    'Thank you for choosing our cafe! Your order : a cup of tea!';
 
 
 
