42dec6822c16c35cb7163fb54a4992eb5b2a4401
Anastasiia Honcharova <72573939+anastasiia2610@users.noreply.github.com>
2020-10-18 16:34:39 +0300

    Add files via upload

---
+++ HillelHW/Honcharova_HW6/js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+  const fib = n => {
+
+    let a = 1, b = 1;
+
+    for(let i = 3; i <= n; i++){
+
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+
+    return b;
+  }
+
