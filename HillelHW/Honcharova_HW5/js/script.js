003091239ef70a9e2b220a24bbabfbe8e028346a
Anastasiia Honcharova <72573939+anastasiia2610@users.noreply.github.com>
2020-10-19 00:33:53 +0300

    Add files via upload

--- HillelHW/Honcharova_HW5/js/script.js
+++ HillelHW/Honcharova_HW5/js/script.js
@@ -2,27 +2,30 @@
 
 const userLogin = prompt('Please, enter your login:');
 
-let login = 'anesthesia';
+let userPassword = prompt('Please, enter your password:');
 
-const userPassword = prompt('Please, enter your password:');
+const login = 'aqua';
 
-let password = '26102000';
+const password = '2000';
 
-const greeting = 'Welcome to our web-site!';
+if (userLogin === login && userPassword === password) {
+    alert('Welcome to our web-site!');
+} else if (userLogin !== login) {
+    alert('Your login incorrect!');
+} else {
+    while (userPassword !== password) {
+        userPassword = prompt('Please, repeat your password:');
 
-const repeatPassword = 'Please, enter the password again';
-
- if (login !== userLogin) {
-    alert('Your login is incorrect');
-} 
-
-if (password !== userPassword) {
-    alert('Your password is incorrect');
-    confirm(repeatPassword);
-    prompt(userPassword);
-} else if (login === userLogin && password === userPassword) {
-    alert(greeting);
+      if (userPassword === password) {
+          alert('Welcome to our web-site!');
+      } else if (userPassword === null) {
+          alert('Your password incorrect!'); break
+      }
+    }
 }
+
+ 
+
 
 
 
