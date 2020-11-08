ff3239732d060fea3a50100904abc55ff9d06809
Anastasiia Honcharova <72573939+anastasiia2610@users.noreply.github.com>
2020-11-08 17:43:10 +0200

    Add files via upload

---
+++ HillelHW/Honcharova_HW13/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function Cat(name, breed) {
+    this.name = name;
+    this.breed = breed;
+
+    this.meow = function () {
+        alert(`Hi, my name is ${name}, MEOW ^-^!`);
+    }
+}
+
+const cat = new Cat ('Aria', 'Siamese');
+
+cat.meow();
+
+
+
+function User(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+
+    this.greet = function () {
+        alert(`Hello, ${firstName} ${lastName}!`)
+    }
+}
+
+const user = new User ('Drake', 'Bell', '34');
+
+user.greet();
+
